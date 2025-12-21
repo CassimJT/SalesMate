a6c82e6cfaa7cdc2314957f0125b489a168b811d
CassimJT <cassimjaffer47@gmail.com>
2025-12-21 02:44:19 +0200

    added splashScreen

--- main.cpp
+++ main.cpp
@@ -12,13 +12,22 @@
 #include "core/sources/reportmanager.h"
 #include <QQuickWindow>
 
+#if defined(Q_OS_ANDROID)
+#include <jni.h>
+#include <QtCore>
+#endif
+
+#include <QTimer>
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QQuickWindow::setDefaultAlphaBuffer(true);
+
     QQmlApplicationEngine engine;
+
     BarcodeEngine barcodeEngine;
-    AndroidSystem *sytstem  = AndroidSystem::instance();
+    AndroidSystem *sytstem = AndroidSystem::instance();
     DatabaseManager *databaseManager = DatabaseManager::instance();
     ExpensesModel expenseModel;
     IncomeModel incomeModel;
@@ -29,7 +38,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationDomain("SalesMate.com");
     QCoreApplication::setApplicationName("SalesMate");
 
-    engine.rootContext()->setContextProperty("Android",sytstem);
+    engine.rootContext()->setContextProperty("Android", sytstem);
     engine.rootContext()->setContextProperty("databaseManager", databaseManager);
     engine.rootContext()->setContextProperty("productFilterModel", databaseManager->getProxyModel());
     engine.rootContext()->setContextProperty("expenseModel", &expenseModel);
@@ -37,7 +46,7 @@ int main(int argc, char *argv[])
     engine.rootContext()->setContextProperty("ServiceModel", &serviceModel);
     engine.rootContext()->setContextProperty("ReportManger", &reportManger);
     engine.rootContext()->setContextProperty("BarcodeEngine", &barcodeEngine);
-    // qmlRegisterType <BarcodeEngine>("Cisociety",1,0,"BarcodeEngine");
+
     qmlRegisterSingletonType(QUrl("qrc:/UI/Stock/SalesModel.qml"), "SalesModel", 1, 0, "SalesModel");
 
     QObject::connect(
@@ -46,7 +55,17 @@ int main(int argc, char *argv[])
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
+
     engine.loadFromModule("SalesMate", "Main");
+
+#if defined(Q_OS_ANDROID)
+    // Show the native splash screen for 4 seconds (adjust as needed)
+    // Then hide it with a nice 400 ms fade-out animation
+    QTimer::singleShot(500, []() {
+        QNativeInterface::QAndroidApplication::hideSplashScreen(400);  // 400 ms fade
+        // Use 0 for instant hide: hideSplashScreen(0)
+    });
+#endif
 
     return app.exec();
 }
